dbe7905f23084941dd036930c233d5e3e4ef2e20
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2018-09-13 11:38:46 -0300

    modificado

--- ejercicio22.php
+++ ejercicio22.php
@@ -4,5 +4,20 @@ if(empty($_GET["checks"])) {
 }else
     echo "Hoy es: " . $_GET['dia'] . "/" . $_GET['mes'] . "/" . $_GET['anio'];
 ?>
+<select>
+        <?php
+                echo "<option selected>".$_GET['dia']."</option>";
+        ?>
+</select>
+<select>
+    <?php
+    echo "<option selected>".$_GET['mes']."</option>";
+    ?>
+</select>
+<select>
+    <?php
+    echo "<option selected>".$_GET['anio']."</option>";
+    ?>
+</select>
 
 <br><br><form action='ejercicio2.php' method='get'><input type='submit' value='Volver'></form>
